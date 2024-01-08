08d93f650fa65a380fccead1dd15ff4205fe8ef9
SarabjitS <35809229+SarabjitS@users.noreply.github.com>
2024-01-08 19:08:04 -0330

    green board works

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -4,12 +4,18 @@ export default function Board() {
   const rows = [8, 7, 6, 5, 4, 3, 2, 1];
   const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+  const getClassName = (i, j) => {
+    let x = "square";
+    x += (i + j) % 2 === 0 ? " square-light" : " square-dark";
+    return x;
+  };
+
   return (
     <div className="board">
       <div className="squares">
-        {rows.map((row) =>
-          cols.map((col) => (
-            <div>
+        {rows.map((row, i) =>
+          cols.map((col, j) => (
+            <div key={row + "-" + col} className={getClassName(i, j)}>
               {row}
               {col}
             </div>
